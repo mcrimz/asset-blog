20716fc1e497fa729144c0484d972ffad584fcaf
mcrimz <37566846+mcrimz@users.noreply.github.com>
2021-04-23 18:22:54 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="insurance-when-is-wor.html"}
-if(x=="2"){xxx="1motorcycle-insurance-comparison-tips.html"}
+if(x=="2"){xxx="motorcycle-insurance-comparison-tips.html"}
 if(x=="3"){xxx="real-estate-finance-comparison-finance.html"}
 if(x=="4"){xxx="investing-money-with-these-7-tips.html"}a_to_ve[i].href="https://www.financeszen.com/2020/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
